7370fbcbbc8c05130f1c9d3959d9216a428c7bc6
ginkgocreate <114222055+ginkgocreate@users.noreply.github.com>
2024-11-20 23:16:50 +0900

    add php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = '掲示板';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('データベース接続に失敗しました: ' . $e->getMessage());
+}
+?>
